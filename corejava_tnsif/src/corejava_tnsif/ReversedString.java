0b20c15ca9befa69a9dd3bb4351fd649d91a3bcf
aparna <masireddyaparna0@gmail.com>
2026-02-13 10:59:34 +0530

    first commit

---
+++ corejava_tnsif/src/corejava_tnsif/ReversedString.java
@@ -0,0 +1,22 @@
+package corejava_tnsif;
+
+import java.util.Scanner;
+
+public class ReversedString {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the String  to reverse:");
+		String word = sc.next();
+		
+		String rev_word = "";
+		for(int i = word.length() - 1; i >= 0; i--) {
+			rev_word += word.charAt(i);
+		}
+		System.out.println("String :" + word);
+		System.out.println("Reversed String :" + rev_word);
+		
+		sc.close();
+	}
+	
+}
